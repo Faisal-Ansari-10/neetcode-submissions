a55fbca3890c0cc84f6dc7b1508eb61e6b11201a
Faisal Ansari <112305981+Faisal-Ansari-10@users.noreply.github.com>
2026-07-23 07:43:51 +0530

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int Trap(int[] height) {
+        int n = height.Length;
+        int[] leftWall = new int[n];
+        int[] rightWall = new int[n];
+
+        leftWall[0] = height[0];
+        rightWall[n - 1] = height[n - 1];
+
+        for (int i = 1; i < n; i++) {
+            leftWall[i] = Math.Max(height[i], leftWall[i - 1]);
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            rightWall[i] = Math.Max(height[i], rightWall[i + 1]);
+        }
+
+        int water = 0;
+
+        for (int i = 0; i < n; i++) {
+            water += Math.Max(Math.Min(leftWall[i], rightWall[i]) - height[i], 0);
+        }
+
+        return water;
+    }
+}
